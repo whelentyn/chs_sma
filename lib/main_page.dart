a6c9e81a8a4f24898081f0b5baedf056176bcf4a
whelentyn <valentin.bogulean@student.upt.ro>
2024-01-02 15:15:52 +0200

    added buttons, searchbar, top and bottom styling for the page

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'auth/login_screen.dart';
@@ -80,42 +81,150 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     String? _email = _auth.currentUser!.email;
     return Scaffold(
-        appBar: AppBar(
-          leading: null,
-          title: const Text("Cooking App"),
-        ),
-        body: ListView.builder(
-          itemCount: _allResults.length,
-          itemBuilder: (context, index) {
-            return Card(
-              shape: RoundedRectangleBorder(
-                side: const BorderSide(
-                  color: Colors.black,  // Set border color
-                  width: 2.0,  // Set border width
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text("Cooking App"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              // Handle the press action. For example, navigate to the profile page.
+              print("Profile icon tapped!"); // Replace with your navigation logic
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CupertinoSearchTextField(
+              onChanged: (value) {
+                // Implement the search logic or handle the text change.
+                print("The search text is: $value");
+              },
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () => print("Breakfast tapped"),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),  // Background color
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),  // Rounded corners
+                        side: const BorderSide(color: Colors.black, width: 2),  // Border
+                      ),
+                    ),
+                    padding: MaterialStateProperty.all(const EdgeInsets.symmetric(vertical: 10, horizontal: 20)), // Padding
+                  ),
+                  child: Text("Breakfast"),
                 ),
-                borderRadius: BorderRadius.circular(8.0),  // Optional: Set border radius
-              ),
-              child: ListTile(
-                leading: _allResults[index]['image_url'] != null
-                    ? Image.network(
-                  _allResults[index]['image_url'],
-                  width: 75,
-                  height: 50,
-                  fit: BoxFit.cover,
-                )
-                    : const SizedBox(width: 50, height: 50),
-                title: Text(
-                  _allResults[index]['name'],
+                const SizedBox(width: 8),
+                TextButton(
+                  onPressed: () => print("Dinner tapped"),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),  // Background color
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),  // Rounded corners
+                        side: const BorderSide(color: Colors.black, width: 2),  // Border
+                      ),
+                    ),
+                    padding: MaterialStateProperty.all(const EdgeInsets.symmetric(vertical: 10, horizontal: 20)), // Padding
+                  ),
+                  child: Text("Dinner"),
                 ),
-                subtitle: Text(
-                  _allResults[index]['description'],
+                const SizedBox(width: 8),
+                TextButton(
+                  onPressed: () => print("Dessert tapped"),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),  // Background color
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),  // Rounded corners
+                        side: const BorderSide(color: Colors.black, width: 2),  // Border
+                      ),
+                    ),
+                    padding: MaterialStateProperty.all(const EdgeInsets.symmetric(vertical: 10, horizontal: 20)), // Padding
+                  ),
+                  child: Text("Dessert"),
                 ),
-                trailing: Text(
-                  _allResults[index]['preparation_time'],
+                const SizedBox(width: 8),
+                TextButton(
+                  onPressed: () => print("Lunch tapped"),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),  // Background color
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),  // Rounded corners
+                        side: const BorderSide(color: Colors.black, width: 2),  // Border
+                      ),
+                    ),
+                    padding: MaterialStateProperty.all(const EdgeInsets.symmetric(vertical: 10, horizontal: 20)), // Padding
+                  ),
+                  child: const Text("Lunch"),
                 ),
-              ),
-            );
-          },
-        ));
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _allResults.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(
+                      color: Colors.black, // Set border color
+                      width: 2.0, // Set border width
+                    ),
+                    borderRadius: BorderRadius.circular(8.0), // Optional: Set border radius
+                  ),
+                  child: ListTile(
+                    leading: _allResults[index]['image_url'] != null
+                        ? Image.network(
+                      _allResults[index]['image_url'],
+                      width: 75,
+                      height: 50,
+                      fit: BoxFit.cover,
+                    )
+                        : const SizedBox(width: 50, height: 50),
+                    title: Text(
+                      _allResults[index]['name'],
+                    ),
+                    subtitle: Text(
+                      _allResults[index]['description'],
+                    ),
+                    trailing: Text(
+                      _allResults[index]['preparation_time'],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Handle the camera icon press action.
+          print("Camera icon tapped!");
+        },  // Camera icon
+        backgroundColor: Colors.white,  // Circle color
+        // The elevation helps in giving the circular shadow confirming it's rounded
+        elevation: 5.0,
+        child: const Icon(Icons.camera_alt),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: const BottomAppBar(
+        color: Colors.amber,  // Amber color for the bottom bar
+        shape: CircularNotchedRectangle(),  // Notch for FloatingActionButton
+        notchMargin: 2.0,  // Margin for the notch
+      ),
+    );
   }
 }
