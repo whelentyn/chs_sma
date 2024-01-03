236f1576665aa47d13393396fc8b1aa3a7c83954
adelinapasculescu1 <87282557+adelinapasculescu1@users.noreply.github.com>
2024-01-03 21:47:24 +0200

    init

---
+++ lib/auth/login_screen.dart
@@ -0,0 +1,134 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cooking_app/auth/signup_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'forgot_password.dart';
+import '../main_page.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+
+  String _email = "";
+  String _password = "";
+
+  void displayMessage(String message, {bool isError = false}) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: isError ? Colors.red : Colors.green,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void handleLogin() async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: _email, password: _password);
+      print("User successfully logged in! ${userCredential.user!.email}");
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const MainPage()));
+    } catch (e) {
+      print("Error during login: $e");
+      displayMessage("Error during login: $e", isError: true);
+    } finally {
+      _emailController.clear();
+      _passController.clear();
+
+      setState(() {
+        _email = "";
+        _password = "";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    label: Text("Email"),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter your email address!";
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      _email = value;
+                    });
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _passController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    label: Text("Password"),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter your password!";
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      _password = value;
+                    });
+                  },
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      handleLogin();
+                    }
+                  },
+                  child: const Text("Login"),
+                ),
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const SignUpScreen())),
+                  child: const Text("Don't have an account? Sign up"),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ForgotPasswordScreen())),
+                  child: const Text("Forgot Password?"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,253 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cooking_app/edit_profile_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'auth/login_screen.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final List<Map<String, dynamic>> _allResults = [];
+  String _selectedCategory = 'All'; // Track the selected category
+  final List<String> _recipeIds = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserRecipes(); // Fetch all recipes initially
+  }
+
+  Future<void> _getUserRecipes({String category = 'All'}) async {
+    try {
+      String? userId = _auth.currentUser?.uid;
+      _recipeIds.clear();
+
+
+      if (userId != null) {
+        var userRecipeDocs = await FirebaseFirestore.instance
+            .collection('UserRecipes')
+            .where('user_id',
+                isEqualTo: FirebaseFirestore.instance.doc('Users/$userId'))
+            .get();
+
+        for (var doc in userRecipeDocs.docs) {
+          var recipeIdList = doc['recipe_id'];
+          if (recipeIdList is List) {
+            for (var recipeId in recipeIdList) {
+              if (recipeId is DocumentReference) {
+                _recipeIds.add(recipeId.id);
+              }
+            }
+          }
+        }
+
+        List<Map<String, dynamic>> userRecipes = [];
+        for (var recipeId in _recipeIds) {
+          var recipeDoc = await FirebaseFirestore.instance
+              .collection('Recipes')
+              .doc(recipeId)
+              .get();
+
+          if (recipeDoc.exists &&
+              (category == 'All' ||
+                  recipeDoc.data()!['category'] == category)) {
+            Map<String, dynamic> recipeData = recipeDoc.data()!;
+            recipeData['id'] = recipeId; // Include the recipeId
+            userRecipes.add(recipeData);
+          }
+        }
+
+        setState(() {
+          _allResults.clear();
+          _allResults.addAll(userRecipes);
+        });
+      } else {
+        print('No user logged in');
+      }
+    } catch (e) {
+      print('An error occurred while retrieving user recipes: $e');
+    }
+  }
+
+  void _onCategoryPressed(String category) {
+    setState(() {
+      _selectedCategory = category;
+    });
+    _getUserRecipes(category: category);
+  }
+
+  Widget _buildCategoryButton(String label) {
+    return TextButton(
+      onPressed: () => _onCategoryPressed(label),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(
+            _selectedCategory == label ? Colors.grey : Colors.white),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+            side: const BorderSide(color: Colors.black, width: 2),
+          ),
+        ),
+        padding: MaterialStateProperty.all(
+            const EdgeInsets.symmetric(vertical: 10, horizontal: 20)),
+      ),
+      child: Text(label),
+    );
+  }
+
+  Future<void> _removeRecipeFromUser(String? recipeId) async {
+    try {
+      String? userId = _auth.currentUser?.uid;
+
+      if (userId != null && recipeId != null) {
+        String fullPath = recipeId.contains('/') ? recipeId : 'Recipes/$recipeId';
+        print("Full path to remove: $fullPath"); // Debug
+
+        var userRecipeQuery = await FirebaseFirestore.instance
+            .collection('UserRecipes')
+            .where('user_id', isEqualTo: FirebaseFirestore.instance.doc('Users/$userId'))
+            .get();
+
+        if (userRecipeQuery.docs.isNotEmpty) {
+          DocumentReference userRecipesRef = userRecipeQuery.docs.first.reference;
+
+          await userRecipesRef.update({
+            'recipe_id': FieldValue.arrayRemove([FirebaseFirestore.instance.doc(fullPath)])
+          });
+
+          print("Recipe with path $fullPath should be removed from user $userId");
+
+          _getUserRecipes(category: _selectedCategory);
+        } else {
+          print("No UserRecipes document found for the user: $userId");
+        }
+      }
+    } catch (e) {
+      print('An error occurred while removing the recipe: $e');
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cooking App"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const EditProfileScreen()));
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 0),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                _buildCategoryButton("All"),
+                const SizedBox(width: 8),
+                _buildCategoryButton("Breakfast"),
+                const SizedBox(width: 8),
+                _buildCategoryButton("Dinner"),
+                const SizedBox(width: 8),
+                _buildCategoryButton("Dessert"),
+                const SizedBox(width: 8),
+                _buildCategoryButton("Lunch"),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Favorite recipes",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _allResults.length,
+              itemBuilder: (context, index) {
+                var recipe = _allResults[index];
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(
+                      color: Colors.black,
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: ListTile(
+                    leading: recipe['image_url'] != null
+                        ? Image.network(
+                      recipe['image_url'],
+                      width: 75,
+                      height: 50,
+                      fit: BoxFit.cover,
+                    )
+                        : const SizedBox(width: 50, height: 50),
+                    title: Text(
+                      recipe['name'] ?? 'Recipe Name',
+                    ),
+                    subtitle: Text(
+                      recipe['description'] ?? 'Description',
+                    ),
+                    trailing: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Text(
+                            recipe['preparation_time'] ?? 'Prep Time',
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.favorite, color: Colors.red),
+                            onPressed: () {
+                              String? recipeId = recipe['id']; // Use the recipe ID directly
+                              if (recipeId != null) {
+                                _removeRecipeFromUser(recipeId);
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        },
+        backgroundColor: Colors.white,
+        elevation: 5.0,
+        child: const Icon(Icons.camera_alt),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: const BottomAppBar(
+        color: Colors.amber,
+        shape: CircularNotchedRectangle(),
+        notchMargin: 2.0,
+      ),
+    );
+  }
+}
