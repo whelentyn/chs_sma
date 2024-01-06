d783067cb210a135a1885c64711dc4f726a9e699
whelentyn <valentin.bogulean@student.upt.ro>
2024-01-06 22:08:13 +0200

    added functionality for tensorflow

--- lib/recipe_handler/photo_screen.dart
+++ lib/recipe_handler/photo_screen.dart
@@ -2,15 +2,27 @@ import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:cooking_app/recipe_handler/ingredients_selector_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
 
 late List<CameraDescription> cameras;
 
+var cameraCount = 0;
+String label = "";
+
+List<String> detectedObjects = [];
 
 Future<void> initCamera() async {
   cameras = await availableCameras();
 }
 
+Future<void> initTflite() async {
+  await Tflite.loadModel(
+      model: "assets/model.tflite",
+      labels: "assets/labels.txt",
+      numThreads: 1,
+      isAsset: true,
+      useGpuDelegate: false);
+}
 
 class PhotoScreen extends StatefulWidget {
   const PhotoScreen({super.key});
@@ -22,11 +34,41 @@ class PhotoScreen extends StatefulWidget {
 class _PhotoScreenState extends State<PhotoScreen> {
   late CameraController _cameraController;
 
+  objectDetector(CameraImage image) async {
+    var detector = await Tflite.runModelOnFrame(
+        bytesList: image.planes.map((e) {
+          return e.bytes;
+        }).toList(),
+        imageHeight: image.height,
+        imageWidth: image.width,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        rotation: 90,
+        numResults: 1,
+        threshold: 0.4,
+        asynch: false);
+    label = detector!.first['label'].toString();
+    if(label.isNotEmpty && !detectedObjects.contains(label)) {
+        detectedObjects.add(label);
+        print(detectedObjects);
+        setState(() {
+          label = detector!.first['label'].toString();
+        });
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     _cameraController = CameraController(cameras[0], ResolutionPreset.max);
     _cameraController.initialize().then((value) {
+      _cameraController.startImageStream((image) {
+        cameraCount++;
+        if(cameraCount % 10 == 0) {
+          cameraCount = 0;
+          objectDetector(image);
+        }
+      });
       if (!mounted) {
         return;
       }
@@ -50,50 +92,63 @@ class _PhotoScreenState extends State<PhotoScreen> {
       ),
       body: _cameraController.value.isInitialized
           ? Stack(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: screenSize.height * 0.05),
-            child: Container(
-              height: screenSize.height * 0.7,
-              width: screenSize.width,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 3,
-                ),
-              ),
-              child: CameraPreview(_cameraController),
-            ),
-          ),
-          Positioned(
-            bottom: 20,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: ElevatedButton.icon(
-                icon: Icon(Icons.camera),
-                label: const Text("Take Photo"),
-                onPressed: () async {
-                  try {
-                    if (_cameraController.value.isInitialized) {
-                      final image = await _cameraController.takePicture();
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              IngredientsPage(),
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: screenSize.height * 0.05),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: screenSize.height * 0.7,
+                        width: screenSize.width,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 3,
+                          ),
                         ),
-                      );
-                    }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
-            ),
-          ),
-        ],
-      )
+                        child: CameraPreview(_cameraController),
+                      ),
+                      if (label != null && label.isNotEmpty)
+                        Positioned(
+                          bottom: 20,
+                          left: 20,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                            color: Colors.white,
+                            child: Text(
+                              '$label',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  left: 0,
+                  right: 0,
+                  child: Center(
+                    child: ElevatedButton.icon(
+                      icon: Icon(Icons.camera),
+                      label: const Text("Take Photo"),
+                      onPressed: () {
+                        _cameraController.dispose();
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => IngredientsPage(),
+                              ),
+                            );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            )
           : Center(child: CircularProgressIndicator()),
     );
   }
